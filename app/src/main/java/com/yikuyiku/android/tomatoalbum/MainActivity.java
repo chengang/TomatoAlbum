e7a2a6a4869cc28364e8ddfb2c70ec28b8e783d6
Gang Chen <missingyousomuch@163.com>
2019-10-08 22:45:19 +0800

    buggy swiperefreshlayout

--- app/src/main/java/com/yikuyiku/android/tomatoalbum/MainActivity.java
+++ app/src/main/java/com/yikuyiku/android/tomatoalbum/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -17,6 +19,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    SwipeRefreshLayout swipeContainer;
+    RecyclerView recyclerView;
     private List<String> stringList = new ArrayList<>();
 
     @Override
@@ -24,16 +28,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        MainRecyclerAdapter mainRecyclerAdapter = new MainRecyclerAdapter(stringList);
+        recyclerView.setAdapter(mainRecyclerAdapter);
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadImages();
+            }
+        });
+    }
+
+    private void loadImages() {
         stringList.add("image1");
         stringList.add("image2");
         stringList.add("pic1");
         stringList.add("pic2");
         stringList.add("photo1");
         stringList.add("photo2");
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
-        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
         MainRecyclerAdapter mainRecyclerAdapter = new MainRecyclerAdapter(stringList);
         recyclerView.setAdapter(mainRecyclerAdapter);
+        Toast.makeText(getApplicationContext(),"Loaded Images", Toast.LENGTH_LONG).show();
+        swipeContainer.setRefreshing(false);
     }
 }
